084e981542fb8f7598d6608c7b202a83d386fd2f
IvanPerezRWS <ivan.perez.rws@gmail.com>
2021-10-23 16:01:43 -0500

    Add Heroku

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 
 import gql from 'graphql-tag';
 
-export const LOGIN_USER = qgl`
+export const LOGIN_USER = gql`
 mutation login($email: String!, $password: String) {
     login(email: $email, password: $password) {
         token
